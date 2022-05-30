309ec97dc53dec2f336090d1ed460f4709489295
Muhammed Oğuz <m.oguz2018@gtu.edu.tr>
2022-05-30 16:34:54 +0300

    Change

--- color-checker/__init__.py
+++ color-checker/__init__.py
@@ -27,7 +27,7 @@ def load_model_and_predict(inp, modelFileName):
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logging.info('Python HTTP trigger function processed  a request.')
 
     headers = {
         "Content-type": "application/json",
